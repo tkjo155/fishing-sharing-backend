cd166d5d7ec08bda0570c43a5a212267ba8b36e3
tkjo155 <takajo.sum15@gmail.com>
2024-05-01 09:33:14 +0900

    validate追加

--- src/lib/updateFishlog.ts
+++ src/lib/updateFishlog.ts
@@ -1,7 +1,25 @@
 import { PrismaClient } from "@prisma/client";
 import { FishLog } from "@prisma/client";
+const MAX_NAME_LENGTH = 50;
+const MAX_SIZE = 1000;
 
 const prisma = new PrismaClient();
+// nameの文字数とsizeについて検証(検証対象を引数に)
+const validateFishLogParameters = (fishName: string, size: number): void => {
+    if (!fishName.length) {
+      throw new Error(`Invalid Parameter: FishName is required`);
+    }
+  
+    if (fishName.length > MAX_NAME_LENGTH) {
+      throw new Error(
+        `Invalid Parameter: FishName must be less than ${MAX_NAME_LENGTH} characters`
+      );
+    }
+  
+    if (size > MAX_SIZE) {
+      throw new Error(`Invalid Parameter: Size must be  ${MAX_SIZE}`);
+    }
+  };
 
 const updateFishlog = async (
     id: number,
@@ -15,7 +33,9 @@ const updateFishlog = async (
     size: number,
     tide: string,
 ): Promise<FishLog> => {
+    //文字列とサイズの数値が制約を満たしているかどうかを検証
     try {
+        validateFishLogParameters(fishName, size);
         const result = await prisma.fishLog.update({
             where: { id: Number(id) },
             data: {
@@ -32,7 +52,7 @@ const updateFishlog = async (
         });
         console.log(result);
         await prisma.$disconnect();
-
+        
         return result;
     } catch (err) {
         await prisma.$disconnect();
